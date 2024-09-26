dc372755c5de8311c546c86fe1aade9f9a87aa26
Estorc <nag.raptorprod@gmail.com>
2024-09-26 23:45:19 +0200

    add files

---
+++ shaders/billboard.fs
@@ -0,0 +1,12 @@
+#version 330 core
+out vec4 FragColor;
+
+in vec2 TexCoords;
+
+uniform sampler2D diffuseMap;
+
+void main()
+{    
+    FragColor = texture(diffuseMap, TexCoords);
+    if (FragColor.a < 0.5) discard;
+}
