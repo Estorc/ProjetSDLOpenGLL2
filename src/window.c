71bd43f32b0ae85d448be9f809afe25c006cfee1
Estorc <51833938+Estorc@users.noreply.github.com>
2025-02-18 02:58:31 +0100

    I asked Chat GPT to fix the SRGB, but I can't test because I'm not Elniefe nor his computer. So I hope it will work ??? (?? - ?? - )

--- src/window.c
+++ src/window.c
@@ -32,6 +32,14 @@ s8 create_window(char *title, s32 x, s32 y, s32 width, s32 height, u32 flags, Wi
         return -1;
     }
 
+    SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_FRAMEBUFFER_SRGB_CAPABLE, 1);
+
     // Disable deprecated functions
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
     // Use double-buffering
