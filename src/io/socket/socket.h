02500c9a60f59034e4bf097c3aa577893c9b4d64
Estorc <51833938+Estorc@users.noreply.github.com>
2025-02-22 03:56:00 +0100

    Added FPS Chess basic gameplay + socket io support

--- src/io/socket/socket.h
+++ src/io/socket/socket.h
@@ -1,15 +1,17 @@
 #ifdef _WIN32
-    #include <winsock2.h>
-    #include <ws2tcpip.h>
-    #pragma comment(lib, "ws2_32.lib")  // Link with Winsock library
-    #define CLOSESOCKET closesocket  // Windows uses closesocket() instead of close()
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")  // Link with Winsock library
+#define CLOSESOCKET closesocket  // Windows uses closesocket() instead of close()
+
 #else
-    #include <sys/select.h>
-    #include <sys/socket.h>
-    #include <netinet/in.h>
-    #include <arpa/inet.h>
-    #include <unistd.h>
-    #define CLOSESOCKET close
+#include <sys/select.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+#define CLOSESOCKET close
 #endif
 
 struct socket_request_listener {
