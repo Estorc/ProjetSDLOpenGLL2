2e3c0bc746a690bc900a72da919c0e3da9c2eae3
Estorc <51833938+Estorc@users.noreply.github.com>
2025-02-28 20:13:36 +0100

    Fixed message handler for server

--- test/fps_chess/server/server.c
+++ test/fps_chess/server/server.c
@@ -357,7 +357,6 @@ int handle_message(int bytes_received, char *msg, void * p, void ** data) {
             PRINT_CLIENT_INFO("deconnected\n");
             kill_client(client);
         } else if (command("G_MSG", msg, &args)) {
-            msg[bytes_received-1] = 0;
             PRINT_SERVER_INFO("%s send message : '%s'\n", client->info.name, msg + 6);
             char * message = malloc(sizeof(char) * (strlen(msg + 6) + strlen(client->info.name) + 10));
             sprintf(message, "G_MSG \"%s\":%s", client->info.name, msg + 6);
@@ -368,7 +367,6 @@ int handle_message(int bytes_received, char *msg, void * p, void ** data) {
             }
             free(message);
         } else {
-            msg[bytes_received-1] = 0;
             PRINT_SERVER_INFO("%s send message : '%s'\n", client->info.name, msg);
             char * message = malloc(sizeof(char) * (strlen(msg) + strlen(client->info.name) + 10));
             sprintf(message, "%c_MSG \"%s\":%s", client->party ? 'P':'G', client->info.name, msg);
