614170adef7b5e423abd72219316fda59b347b74
Estorc <51833938+Estorc@users.noreply.github.com>
2025-02-28 18:58:57 +0100

    Fixed message handler for server

--- test/fps_chess/server/server.c
+++ test/fps_chess/server/server.c
@@ -362,7 +362,7 @@ int handle_message(int bytes_received, char *msg, void * p, void ** data) {
             char * message = malloc(sizeof(char) * (strlen(msg + 6) + strlen(client->info.name) + 9));
             sprintf(message, "G_MSG \"%s\":%s", client->info.name, msg + 6);
             for (int j = 0; j < MAX_CLIENTS; j++) {
-                if (clients[j].socket && clients[j].authorized && clients[j] != client) {
+                if (clients[j].socket && clients[j].authorized && clients[j].socket != client->socket) {
                     send_message_with_separator(&clients[j], message);
                 }
             }
@@ -374,13 +374,13 @@ int handle_message(int bytes_received, char *msg, void * p, void ** data) {
             sprintf(message, "%c_MSG \"%s\":%s", client->party ? 'P':'G', client->info.name, msg);
             if (client->party) {
                 for (int j = 0; j < MAX_PARTY_CLIENTS; j++) {
-                    if (client->party->clients[j] && client->party->clients[j] != client) {
+                    if (client->party->clients[j] && client->party->clients[j]->socket != client->socket) {
                         send_message_with_separator(client->party->clients[j], message);
                     }
                 }
             } else {
                 for (int j = 0; j < MAX_CLIENTS; j++) {
-                    if (clients[j].socket && clients[j].authorized && clients[j] != client) {
+                    if (clients[j].socket && clients[j].authorized && clients[j].socket != client->socket) {
                         send_message_with_separator(&clients[j], message);
                     }
                 }
