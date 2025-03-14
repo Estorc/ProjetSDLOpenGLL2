b47746e5f1290a9f08922d2ce6aa8a2051c88575
Ekamyl <ekremay@hotmail.fr>
2025-03-14 21:16:13 +0100

    passage au rgb plutot que srgb

--- src/render/framebuffer.c
+++ src/render/framebuffer.c
@@ -25,7 +25,7 @@ void create_msaa_framebuffer(MSAA *msaa) {
     // create a multisampled color attachment texture
     glGenTextures(1, &msaa->textureColorBufferMultiSampled);
     glBindTexture(GL_TEXTURE_2D_MULTISAMPLE, msaa->textureColorBufferMultiSampled);
-    glTexImage2DMultisample(GL_TEXTURE_2D_MULTISAMPLE, 4, GL_SRGB, window_width, window_height, GL_TRUE);
+    glTexImage2DMultisample(GL_TEXTURE_2D_MULTISAMPLE, 4, GL_RGB, window_width, window_height, GL_TRUE);
     glBindTexture(GL_TEXTURE_2D_MULTISAMPLE, 0);
     glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D_MULTISAMPLE, msaa->textureColorBufferMultiSampled, 0);
     // create a (also multisampled) renderbuffer object for depth and stencil attachments
